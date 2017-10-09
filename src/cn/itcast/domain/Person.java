b6ad17d850d599b83c001fb4d802ca1aa8dd59ea
shrimp_xue <shrimp_xue@163.com>
2017-10-09 12:01:39 +0800

    初始化父类

--- src/cn/itcast/domain/Person.java
+++ src/cn/itcast/domain/Person.java
@@ -1,5 +1,27 @@
 package cn.itcast.domain;
 
 public class Person {
+	private String name;
+	private Integer age;
+
+	public Person() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 }
